41c51df9839a8ee0965e6033e045a1e899524182
L-Edgar <lelishaedgar10@gmail.com>
2024-05-08 15:32:30 +0300

    whitenoise

--- blooddonationtesting/settings.py
+++ blooddonationtesting/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,12 +141,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-os.path.join(BASE_DIR,"static")
- ]
-STATIC_ROOT=os.path.join(BASE_DIR,'assets')
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+#STATICFILES_DIRS=[
+#os.path.join(BASE_DIR,"static")
+# ]
+STATIC_ROOT=BASE_DIR/'assets'
+#STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+#MEDIA_URL='/media/'
+#MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL='/afterlogin'
 
 #for contact us give your gmail id and password
